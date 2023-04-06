a78b29005a85f9003aa20da4532b80ab3ad176aa
Vinícius da Silva Ribeiro <59255818+Vini92@users.noreply.github.com>
2023-04-05 23:50:59 -0300

    Adicionando comentários e um laço de repetição

--- BMExercicio05.java
+++ BMExercicio05.java
@@ -1,15 +1,24 @@
 /* Livro Batista & Moraes (2013): questão 5 da página 44;
 Implemente um programa que recebe um número de 1 a 7 e imprime o
-dia da semana correspondente.
-*/
+dia da semana correspondente. */
 
 import java.util.Scanner;
 
 public class BMExercicio05 {
     public static void main(String args[]) {
-        Scanner read = new Scanner(System.in);
-        int numero = read.nextInt();
+
+        int numero = 0;
+
+        // Repetirá enquanto não digitar um número válido
+        while (numero < 1 || numero > 7) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Informe um número entre 1 e 7: ");
+            numero = scanner.nextInt();
+        }        
+
         String dia_da_semana = "";
+        
+        // Exibirá o dia da semana correspondente ao número
         switch(numero) {
             case 1:
                 dia_da_semana = "domingo";
